8d950774b84ad5b023dda1e2224f4732571cde18
saswesley <saswesley@outlook.com>
2022-09-15 13:12:07 -0300

    Hello World

--- src/main/java/io/github/saswesley/VendasApplication.java
+++ src/main/java/io/github/saswesley/VendasApplication.java
@@ -2,9 +2,17 @@ package io.github.saswesley;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class VendasApplication {
+	
+	@GetMapping("/hello")
+	public String helloWorld () {
+		return "Hello World";
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
