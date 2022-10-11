511885b892ba96c7c9880153708191021113edc9
saswesley <saswesley@outlook.com>
2022-10-11 13:19:25 -0300

    Implementando JPA

--- src/main/java/io/github/saswesley/domain/entity/Cliente.java
+++ src/main/java/io/github/saswesley/domain/entity/Cliente.java
@@ -1,8 +1,23 @@
 package io.github.saswesley.domain.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "cliente")
 public class Cliente {
 	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	@Column (name = "id")
 	private Integer id;
+
+	
+	@Column (name = "nome", length = 100)
 	private String nome;
 	
 	public Cliente() {
--- src/main/java/io/github/saswesley/domain/repositorio/Clientes.java
+++ src/main/java/io/github/saswesley/domain/repositorio/Clientes.java
@@ -1,68 +1,64 @@
 package io.github.saswesley.domain.repositorio;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.github.saswesley.domain.entity.Cliente;
 
 @Repository
 public class Clientes {
 	
-	private static String INSERT = "INSERT INTO cliente (nome) VALUES (?)";
-	private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
-	private static String UPDATE = "UPDATE cliente SET nome = ? WHERE id = ? ";
-	private static String DELETE = "DELETE FROM cliente WHERE id = ?";
 	
-	@Autowired		
-	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private EntityManager entityManager;
 	
+	@Transactional
 	public Cliente salvar (Cliente cliente) {
-		jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+		entityManager.persist(cliente);
 		return cliente;
 	}
 	
+	@Transactional
 	public Cliente atualizar (Cliente cliente) {
-		jdbcTemplate.update(UPDATE, new Object[] {
-				cliente.getNome(), cliente.getId()
-				});
+		entityManager.merge(cliente);
 		return cliente;
 	}
 	
+	@Transactional
 	public void deletar (Cliente cliente) {
-		deletar(cliente.getId());
+		if(!entityManager.contains(cliente)) {
+			cliente = entityManager.merge(cliente);
+		}
+		entityManager.remove(cliente);
 	}
 	
+	@Transactional
 	public void deletar(Integer id) {
-		jdbcTemplate.update(DELETE, new Object[] {id});
+		Cliente cliente = entityManager.find(Cliente.class, id);
+		deletar(cliente);
 	}
 	
+	@Transactional(readOnly = true)
 	public List<Cliente> buscarPorNome(String nome) {
-		return jdbcTemplate.query(SELECT_ALL.concat("where nome like ?"), 
-				new Object[]{"%" + nome + "%"},
-				obterClienteMapper());
+		String jpql = "select c from Cliente c where c.nome like :nome";
+		TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+		query.setParameter("nome", "%" + nome + "%");
+		return query.getResultList();
 	}
 	
+	@Transactional
 	public List<Cliente> obterTodos(){
-		return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+		return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
 		
 	}	
 		
-	public RowMapper<Cliente> obterClienteMapper(){
-		return new RowMapper<Cliente>() {
-			@Override
-			public Cliente mapRow (ResultSet resultSet, int i) throws SQLException{
-				Integer id = resultSet.getInt("id");
-				String nome = resultSet.getString("nome");
-				return new Cliente(id, nome);
-			}
-		};
-	}
+
 		
 }
