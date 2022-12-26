7c69d8029c34bf0dc7c45d8aec2bbe3cb87b8403
saswesley <saswesley@outlook.com>
2022-12-26 18:16:42 -0300

    Autorização de URLS

--- src/main/java/io/github/saswesley/config/SecurityConfig.java
+++ src/main/java/io/github/saswesley/config/SecurityConfig.java
@@ -18,12 +18,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		super.configure(auth);
+		auth.inMemoryAuthentication()
+					.passwordEncoder(passwordEnconder())
+					.withUser("Fulano")
+					.password(passwordEnconder().encode("123"))
+					.roles("USER");
 	}
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http
+			.csrf().disable()
+			.authorizeRequests()
+				.antMatchers("/api/clientes/**")
+					.authenticated()
+					//.hasRole("USER");
+			.and()
+				.formLogin(); //É possivel colocar um formulário "/meu-login.html"
+		;	
 	}
 	
 }
